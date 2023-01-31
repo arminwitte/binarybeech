b9280793840f39fa7569061871b66431cc6c9fb5
arminwitte <110226001+arminwitte@users.noreply.github.com>
2023-01-31 07:01:03 +0100

    Update utils.py

--- binarybeech/utils.py
+++ binarybeech/utils.py
@@ -1,16 +1,16 @@
- import numpy as np
+import numpy as np
 
-    def _precision(m):
-        return np.diag(m) / np.sum(m, axis=1)
+def precision(m):
+    return np.diag(m) / np.sum(m, axis=1)
         
-    def _recall(m):
-        return np.diag(m) / np.sum(m, axis=0)
+def recall(m):
+    return np.diag(m) / np.sum(m, axis=0)
 
-    def _F1(P,R):
-        #F = np.zeros_like(P)
-        #for i in range(len(
-        return 2 * P * R / (P + R)
+def F1(P,R):
+    #F = np.zeros_like(P)
+    #for i in range(len(
+    return 2 * P * R / (P + R)
     
-    def _accuracy(m):
-        return np.sum(np.diag(m))/np.sum(np.sum(m))
+def accuracy(m):
+    return np.sum(np.diag(m))/np.sum(np.sum(m))
         
