d7cfd8a9dd836a014a557beca5de5253e6ab9459
arminwitte <110226001+arminwitte@users.noreply.github.com>
2023-02-01 11:05:05 +0100

    Populate metrics

--- binarybeech/metrics.py
+++ binarybeech/metrics.py
@@ -1,7 +1,86 @@
-# Created wirh ChatGPT
+# Initially Created with ChatGPT
 from abc import ABC, abstractmethod
 
 class Metric(ABC):
+    def __init__(self,y_name):
+        self.y_name = y_name
+
+    def _y(self,df):
+        return df[self.y_name].values
+
+    def _gini_impurity(self, df):
+        unique, counts = np.unique(df[self.y_name].values, return_counts=True)
+        N = df[self.y_name].values.ravel().size
+        p = counts/N
+        #print(unique)
+        #print(p)
+        return 1. - np.sum(p**2)
+    
+    def _shannon_entropy(self,df):
+        unique, counts = np.unique(df[self.y_name].values, return_counts=True)
+        N = df[self.y_name].values.size
+        p = counts/N
+        return -np.sum(p * np.log2(p))
+
+    def _misclassification_cost(self,df):
+        y = df[self.y_name].values
+        unique, counts = np.unique(y, return_counts=True)
+        N = y.size
+        p = np.max(counts)/N
+        return 1. - p
+    
+    def _logistic_loss(self,df):
+        y = df[self.y_name].values
+        #unique, counts = np.unique(y, return_counts= True)
+        #count_max = np.max(counts)
+        p_ = self._node_value(df) #np.nanmax(counts)/np.sum(counts)
+        p_ = np.clip(p_,1e-12,1.-1e-12)
+        p = np.ones_like(y) * p_
+        l = np.sum(-y*np.log(p)-(1-y)*np.log(1-p))
+        return l
+    
+    def _mean_squared_error(self,df):
+        y = df[self.y_name].values
+        y_hat = self._node_value(df)
+        e = y - y_hat
+        return 1/e.size * (e.T @ e)
+    
+    def _mean(self,df):
+        return np.nanmean(df[self.y_name].values)
+    
+    def _majority_class(self,df):
+        y = df[self.y_name].values
+        unique, counts = np.unique(y,return_counts=True)
+        ind_max = np.argmax(counts)
+        return unique[ind_max]
+    
+    def _odds(self,df):
+        y = df[self.y_name].values
+        unique, counts = np.unique(y, return_counts=True)
+        d={0:0,1:0}
+        for i, u in enumerate(unique):
+            d[u] = counts[i]
+        if d[0] == 0:
+            return np.Inf
+        odds = d[1]/d[0]
+        #print(f"odds: {odds}")
+        return odds
+    
+    def _log_odds(self,df):
+        odds = self._odds(df)
+        odds = np.clip(odds,1e-12,1e12)
+        logodds = np.log(odds)
+        #print(f"logodds: {logodds}")
+        return logodds
+    
+    def _probability(self,df):
+        odds = self._odds(df)
+        if odds == np.Inf:
+            return 1.
+        p = odds/(1+odds)
+        #print(f"odds: {odds:.2f} probability: {p:.4f}")
+        return p
+
     @abstractmethod
     def loss(self, data):
         pass
@@ -15,6 +94,9 @@ class Metric(ABC):
         pass
 
 class RegressionMetrics(Metric):
+    def __init__(self,y_name):
+        super().__init__(y_name)
+
     def loss(self, data):
         # Implementation of the loss calculation for regression
         pass
@@ -28,6 +110,9 @@ class RegressionMetrics(Metric):
         pass
 
 class LogisticMetrics(Metric):
+    def __init__(self,y_name):
+        super().__init__(y_name)
+
     def loss(self, data):
         # Implementation of the loss calculation for logistic
         pass
@@ -41,6 +126,9 @@ class LogisticMetrics(Metric):
         pass
 
 class ClassificationMetrics(Metric):
+    def __init__(self,y_name):
+        super().__init__(y_name)
+
     def loss(self, data):
         # Implementation of the loss calculation for classification
         pass
@@ -67,9 +155,9 @@ class MetricFactory:
             raise ValueError("Invalid metric type")
 
 factory = MetricFactory()
-factory.register("RegressionMetrics", RegressionMetrics)
-factory.register("LogisticMetrics", LogisticMetrics)
-factory.register("ClassificationMetrics", ClassificationMetrics)
+factory.register("regression", RegressionMetrics)
+factory.register("logistic", LogisticMetrics)
+factory.register("classification", ClassificationMetrics)
 
 class MainClass:
     def __init__(self, metric_type):
