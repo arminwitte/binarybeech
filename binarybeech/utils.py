07586ce5af6335317439325c60182317bf269a8e
arminwitte <110226001+arminwitte@users.noreply.github.com>
2023-02-13 20:44:54 +0100

    Bugfix

--- binarybeech/utils.py
+++ binarybeech/utils.py
@@ -4,5 +4,5 @@ def print_bars(d, max_width=70):
     max_val = max(d.values)
     for key, val in d.items:
         l = int(round(50 * val / max_val))
-        print(f"{key:10}|{"#"*l}{}{val:4.2}
+        print(f"{key:10}|{"#"*l}{}{val:4.2})
     
