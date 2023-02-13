Bugfix, scaling

binarybeech/binarybeech.py
@@ -1,4 +1,4 @@
-K#!/usr/bin/env python
+#!/usr/bin/env python
 # coding: utf-8
 
 import pandas as pd
@@ -643,6 +643,9 @@ class RandomForest:
                 R_children = np.sum([b.pinfo["R"] for b in n.branches])
                 R_delta = R_parent - R_children
                 d[name] += R_delta
+        max_val = max(d.values)
+        for key in d.keys:
+            d[key] /= max_val
         return d
             
          