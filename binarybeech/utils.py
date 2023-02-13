d0fa173e899cf8799bda49c63922c475d07623da
arminwitte <110226001+arminwitte@users.noreply.github.com>
2023-02-13 20:41:51 +0100

    Scaling

--- binarybeech/utils.py
+++ binarybeech/utils.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 def print_bars(d, max_width=70):
-    for key, val in d.items():
-        l = int(round(50 * val))
+    max_val = max(d.values)
+    for key, val in d.items:
+        l = int(round(50 * val / max_val))
         print(f"{key:10}|{"#"*l}{}{val:4.2}
     
