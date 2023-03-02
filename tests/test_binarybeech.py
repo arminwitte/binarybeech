739db251051c1cffebaa6c23e9e4aefe7a567948
witte.armin@gmail.com <witte.armin@gmail.com>
2023-03-02 10:04:36 +0100

    test gbt

--- tests/test_binarybeech.py
+++ tests/test_binarybeech.py
@@ -1,6 +1,15 @@
 from binarybeech.binarybeech import *
+import pandas as pd
 
 
 def test_node():
     n = Node(value=1.0)
     assert n.is_leaf
+    
+def test_gbt():
+    df = pd.read_csv("data/titanic.csv")
+    gbt = GradientBoostedTree(df_titanic,"Survived",learning_rate=0.5)
+    gbt.train(10)
+    p = gbt._predict(df.iloc[0])
+    assert p == 0.9
+    
