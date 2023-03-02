19b8fac872747b8c8773ea334dc1e33d435f40ae
witte.armin@gmail.com <witte.armin@gmail.com>
2023-03-02 11:41:55 +0100

    validation

--- tests/test_binarybeech.py
+++ tests/test_binarybeech.py
@@ -11,19 +11,21 @@ def test_cart():
     c = CART(df_titanic,"Survived", metrics_type="classification")
     c.create_tree()
     p = c._predict(df_titanic.iloc[0])
-    assert p == 0.9
+    print(c.validate())
+    assert p == 0
     
 def test_cart2():
     df_titanic = pd.read_csv("data/titanic.csv")
     c = CART(df_titanic,"Survived", metrics_type="classification")
     c.train()
     p = c._predict(df_titanic.iloc[0])
-    assert p == 0.9
+    print(c.validate())
+    assert p == 0
     
     
 def test_gbt():
     df_titanic = pd.read_csv("data/titanic.csv")
-    gbt = GradientBoostedTree(df_titanic,"Survived",learning_rate=0.5)
+    gbt = GradientBoostedTree(df_titanic,"Survived",learning_rate=0.5,init_metrics_type="logistic")
     gbt.train(10)
     p = gbt._predict(df_titanic.iloc[0])
     assert p == 0.9
