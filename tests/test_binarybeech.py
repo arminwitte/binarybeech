6b592e1751663c5a28b340064845935b5a377979
witte.armin@gmail.com <witte.armin@gmail.com>
2023-03-05 09:54:31 +0100

    fix tests

--- tests/test_binarybeech.py
+++ tests/test_binarybeech.py
@@ -30,18 +30,18 @@ def test_gbt():
     df_titanic = pd.read_csv("data/titanic.csv")
     gbt = GradientBoostedTree(df_titanic,"Survived",learning_rate=0.5,init_metrics_type="logistic")
     gbt.train(10)
-    p = c.predict(df_titanic)
-    val = c.validate()
+    p = gbt.predict(df_titanic)
+    val = gbt.validate()
     acc = val["accuracy"]
-    np.testing.assert_allclose(p[:10], [0, 1, 1, 1, 0, 0, 0, 0, 1, 1])
+    np.testing.assert_allclose(np.round(p[:10]).astype(int), [0, 1, 1, 1, 0, 0, 0, 0, 1, 1])
     assert acc < 1. and acc > 0.8
     
 def test_randomforest():
     df_titanic = pd.read_csv("data/titanic.csv")
-    rf = RandomForest(df_titanic,"Survived",metrics_type="logistic")
+    rf = RandomForest(df_titanic,"Survived",metrics_type="classification")
     rf.train(10)
-    p = c.predict(df_titanic)
-    val = c.validate_oob()
+    p = rf.predict(df_titanic)
+    val = rf.validate_oob()
     acc = val["accuracy"]
     np.testing.assert_allclose(p[:10], [0, 1, 1, 1, 0, 0, 0, 0, 1, 1])
     assert acc < 1. and acc > 0.8
