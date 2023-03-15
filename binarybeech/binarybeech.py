5413a6793dba235a779f2568383bcb20d3aa03a9
witte.armin@gmail.com <witte.armin@gmail.com>
2023-03-15 10:01:44 +0100

    bugfix

--- binarybeech/binarybeech.py
+++ binarybeech/binarybeech.py
@@ -318,7 +318,7 @@ class CART(Model):
                 metrics = self.validate(df=test_set)
                 #for key, val in metrics.items():
                 #    d["".join((key,"_cv"))].append(val)
-                d["goodness_of_fit"] = self.goodness_of_fit(df=test_set)
+                d["goodness_of_fit"].append(self.goodness_of_fit(df=test_set))
             d["alpha"].append(alpha)
             d["n_leafs"].append(N)
             d["R"].append(R)
