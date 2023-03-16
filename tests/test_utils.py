eb0d42983a8619dae2227183824015d75118f76e
witte.armin@gmail.com <witte.armin@gmail.com>
2023-03-16 10:25:53 +0100

    test model_missings

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,4 +4,9 @@ from binarybeech.utils import model_misings
 
 
 def test_model_missings():
-    pass
+    df_titanic = pd.read_csv("data/titanic.csv")
+    
+    df_new = model_missings(df_titanic,"Survived",X_names=["Age"])
+    
+    has_missings = df_new.isnull().any()
+    assert has_missings["Age"] == False
