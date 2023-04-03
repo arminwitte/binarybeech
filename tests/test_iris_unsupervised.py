262c87a6914e4275ef28d0742e3acdb8e00819b7
witte.armin@gmail.com <witte.armin@gmail.com>
2023-04-03 12:03:57 +0200

    fix test

--- tests/test_iris_unsupervised.py
+++ tests/test_iris_unsupervised.py
@@ -5,7 +5,8 @@ from binarybeech.binarybeech import CART, GradientBoostedTree, RandomForest
 
 
 def test_iris_cart_create():
-    df_iris = pd.read_csv("data/iris.csv")
+    df_iris_orig = pd.read_csv("data/iris.csv")
+    df_iris = df_iris_orig.drop(columns=["species"])
     c = CART(df_iris, None, metrics_type="clustering")
     c.create_tree()
     p = c.predict(df_iris)
