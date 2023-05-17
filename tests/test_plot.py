f3fbd6d6340461356d5250f8f1140205364f5dfb
witte.armin@gmail.com <witte.armin@gmail.com>
2023-05-17 14:00:36 +0200

    test plotting

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -2,9 +2,17 @@ import numpy as np
 import pandas as pd
 
 from binarybeech.binarybeech import CART
+from binarybeech.plot import plot_areas, plot_pruning_quality
 
 
-def test_iris_cart_train():
+def test_plot_areas():
+    df_iris = pd.read_csv("data/iris.csv")
+    c = CART(df=df_iris, y_name="species", metrics_type="classification", seed=42)
+    c.train()
+    plot_areas(c,"petal_width","petal_length",df=df_iris)
+    
+
+def test_plot_pruning_quality():
     df_iris = pd.read_csv("data/iris.csv")
     c = CART(df=df_iris, y_name="species", metrics_type="classification", seed=42)
     c.train()
