164111f788b62ecaae4b988fcf04e1f2616fc622
witte.armin@gmail.com <witte.armin@gmail.com>
2023-05-30 00:22:47 +0200

    bugfix II

--- tests/test_minimizer.py
+++ tests/test_minimizer.py
@@ -11,7 +11,7 @@ def test_scalarsimulatedannealing():
     m = ScalarSimulatedAnnealing()
     x, y = m.minimize(np.sin,0.,7.)
     assert np.isclose(x, 4.7123889715201255, rtol=0.01)
-    assert np.isclose(y, -1., rtol=0.01
+    assert np.isclose(y, -1., rtol=0.01)
     
     
 def test_minimize():
