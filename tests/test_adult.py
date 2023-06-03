22c52f2f3788a40aa16d32dba093439f0576c7fd
witte.armin@gmail.com <witte.armin@gmail.com>
2023-06-03 18:08:11 +0200

    testing high cardinality solution with adult dataset

---
+++ tests/test_adult.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+from binarybeech.binarybeech import CART
+from minarybeech.attributehandler import HighCardinalityNominalAttributeHandler
+
+def test_adult():
+    adult_train = pd.read_csv("data/adult_data.csv", header=None)
+    adult_test = pd.read_csv("data/adult_test.csv", skiprows=1, header=None)
+    c = CART(df=adult_train, y_name=14, max_depth=4, method="classification")
+    c.create_tree()
+    val = c.validate(adult_test)
+    assert val["accuracy"] > 0.82
+    assert isinstance(c.dmgr.data_handlers[1],HighCardinalityNominalAttributeHandler)
