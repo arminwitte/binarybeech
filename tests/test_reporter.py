43351601e93f9840c23ed316004ea3251939192b
witte.armin@gmail.com <witte.armin@gmail.com>
2023-07-03 11:10:40 +0200

    test fixed

--- tests/test_reporter.py
+++ tests/test_reporter.py
@@ -3,7 +3,7 @@ from binarybeech.reporter import Reporter, reporter
 
 def test_reporter_class():
     reporter = Reporter()    
-    reporter.labels(["col1", "col2"])
+    reporter.reset(["col1", "col2"])
     for i in range(10):
         reporter["col1"] = i
         reporter["col2"] = i**2
@@ -11,7 +11,7 @@ def test_reporter_class():
     assert 0 == 0
     
 def test_reporter():
-    reporter.labels(["col1", "col2"])
+    reporter.reset(["col1", "col2"])
     for i in range(10):
         reporter["col1"] = i
         reporter["col2"] = i**2
